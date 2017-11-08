16482e6353cc68c3d538d5b3fa7407e05db5aef8
erth <erik.thunberg@adeprimo.se>
2017-11-08 23:53:50 +0100

    Soon.. sooon.

--- test/test.js
+++ test/test.js
@@ -10,15 +10,19 @@ Mongoose.Promise = require('bluebird');
 const db = Mongoose.connect(process.env.MONGO_URL + process.env.MONGO_DB, {
     useMongoClient: true
 }, (error) => {
-    if (error)
+    if (error) {
         throw error;
+    }
 });
 
 experiment('hapi-users-plugin', () => {
-
     let server;
     let testUserId;
     let testUserJwt;
+    let testUserNewJwt;
+    let createUserId;
+    let createdUserJwt;
+    let getUsersFromId;
     
     const testUser = {
         username: 'testuser',
@@ -168,7 +172,7 @@ experiment('hapi-users-plugin', () => {
             expect(response.result.userCreated).to.be.true();
             expect(response.result.userId).to.be.a.string();
 
-            testUserId = response.result.userId;
+            createUserId = response.result.userId;
         });
     });
 
@@ -190,7 +194,7 @@ experiment('hapi-users-plugin', () => {
         });
     });
     
-    test('Fail to edit another user as admin - No username', () => {
+    test('Fail to edit another user with no username -  as admin', () => {
         return server.inject({
             method: 'PUT',
             url: '/api/users',
@@ -198,7 +202,7 @@ experiment('hapi-users-plugin', () => {
                 'Authorization': testUserJwt
             },
             payload: {
-                id: testUserId,
+                id: createUserId,
                 username: ''
             }
         }).then((response) => {
@@ -207,7 +211,7 @@ experiment('hapi-users-plugin', () => {
         });
     });
 
-    test('Fail to edit another user as admin - No password', () => {
+    test('Fail to edit another user with no password - as admin', () => {
         return server.inject({
             method: 'PUT',
             url: '/api/users',
@@ -215,7 +219,7 @@ experiment('hapi-users-plugin', () => {
                 'Authorization': testUserJwt
             },
             payload: {
-                id: testUserId,
+                id: createUserId,
                 password: ''
             }
         }).then((response) => {
@@ -224,7 +228,7 @@ experiment('hapi-users-plugin', () => {
         });
     });
 
-    test('Fail to edit another user as admin - Username allready taken', () => {
+    test('Fail to edit username on user with usernames that is already taken - as admin', () => {
         return server.inject({
             method: 'PUT',
             url: '/api/users',
@@ -232,7 +236,7 @@ experiment('hapi-users-plugin', () => {
                 'Authorization': testUserJwt
             },
             payload: {
-                id: testUserId,
+                id: createUserId,
                 username: 'nangijala'
             }
         }).then((response) => {
@@ -241,7 +245,7 @@ experiment('hapi-users-plugin', () => {
         });
     });
     
-    test('Edit another user as admin', () => {
+    test('Edit another user - as admin', () => {
         return server.inject({
             method: 'PUT',
             url: '/api/users',
@@ -249,7 +253,7 @@ experiment('hapi-users-plugin', () => {
                 'Authorization': testUserJwt
             },
             payload: {
-                id: testUserId,
+                id: createUserId,
                 username: 'superdeath'
             }
         }).then((response) => {
@@ -258,11 +262,239 @@ experiment('hapi-users-plugin', () => {
         });
     });
 
-    /*test('', () => {
+    test('Edit own user - as admin', () => {
+        return server.inject({
+            method: 'PUT',
+            url: '/api/users',
+            headers: {
+                'Authorization': testUserJwt
+            },
+            payload: {
+                password: 'newtestpassword'
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.result.userUpdated).to.be.true();
+            expect(response.result.newToken).to.be.true();
+
+            testUserNewJwt = response.headers.authorization;
+        });
+    });
+
+    test('Fail to edit own user - as admin with old token', () => {
+        return server.inject({
+            method: 'PUT',
+            url: '/api/users',
+            headers: {
+                'Authorization': testUserJwt
+            },
+            payload: {
+                password: 'testpassword'
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(401);
+            expect(response.result.error).to.be.a.string();
+        });
+    });
+
+    test('Login user - with default scope', () => {
+        return server.inject({
+            method: 'POST',
+            url: '/api/users/authenticate',
+            payload: {
+                username: 'superdeath',
+                password: 'nangijala'
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.headers.authorization).to.be.a.string();
+            createdUserJwt = response.headers.authorization;
+        });
+    });
+
+    test('Fail to edit another user - as normal user', () => {
+        return server.inject({
+            method: 'PUT',
+            url: '/api/users',
+            headers: {
+                'Authorization': createdUserJwt
+            },
+            payload: {
+                id: testUserId,
+                username: 'supertestuser'
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(401);
+            expect(response.result.error).to.be.a.string();
+        });
+    });
+
+    test('Fail to get user with wrong username', () => {
+        return server.inject({
+            method: 'GET',
+            url: '/api/users/death',
+            headers: {
+                'Authorization': createdUserJwt
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(400);
+            expect(response.result.error).to.be.a.string();
+        });
+    });
+
+    test('Get user', () => {
+        return server.inject({
+            method: 'GET',
+            url: '/api/users/superdeath',
+            headers: {
+                'Authorization': createdUserJwt
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.result.user.username).to.be.a.string();
+            expect(response.result.user.password).to.be.undefined();
+            expect(response.result.user.username).to.equal('superdeath');
+        });
+    });
+
+    test('Fail to get users with from-param set to invalid id', () => {
+        return server.inject({
+            method: 'GET',
+            url: '/api/users?from=invalidid',
+            headers: {
+                'Authorization': createdUserJwt
+            },
+        }).then((response) => {
+            expect(response.statusCode).to.equal(400);
+            expect(response.result.error).to.be.a.string();
+        });
+    });
+
+    test('Get users, exactly 3 of them', () => {
+        return server.inject({
+            method: 'GET',
+            url: '/api/users',
+            headers: {
+                'Authorization': createdUserJwt
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.result.users).to.be.an.array();
+            expect(response.result.users.length).to.equal(3);
+            getUsersFromId = response.result.users[0]._id;
+        });
+    });
+
+    test('Limit users to 1', () => {
+        return server.inject({
+            method: 'GET',
+            url: '/api/users?limit=1',
+            headers: {
+                'Authorization': createdUserJwt
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.result.users).to.be.an.array();
+            expect(response.result.users.length).to.equal(1);
+        });
+    });
+
+    test('Get users from ID', () => {
+        return server.inject({
+            method: 'GET',
+            url: '/api/users?from=' + getUsersFromId,
+            headers: {
+                'Authorization': createdUserJwt
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.result.users).to.be.an.array();
+            expect(response.result.users.length).to.equal(2);
+        });
+    });
+
+    test('Fail to remove own user - as admin', () => {
+        return server.inject({
+            method: 'DELETE',
+            url: '/api/users',
+            headers: {
+                'Authorization': testUserNewJwt
+            },
+        }).then((response) => {
+            expect(response.statusCode).to.equal(400);
+            expect(response.result.error).to.be.a.string();
+        });
+    });
+
+    test('Fail to remove user - as normal user', () => {
+        return server.inject({
+            method: 'DELETE',
+            url: '/api/users',
+            headers: {
+                'Authorization': createdUserJwt
+            },
+            payload: {
+                id: testUserId
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(403);
+            expect(response.result.error).to.be.a.string();
+        });
+    });
+
+    test('Fail to remove user with invalid id - as admin', () => {
+        return server.inject({
+            method: 'DELETE',
+            url: '/api/users',
+            headers: {
+                'Authorization': testUserNewJwt
+            },
+            payload: {
+                id: 'randomid'
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(400);
+            expect(response.result.error).to.be.a.string();
+        });
+    });
+
+    test('Remove user - as admin', () => {
+        return server.inject({
+            method: 'DELETE',
+            url: '/api/users',
+            headers: {
+                'Authorization': testUserNewJwt
+            },
+            payload: {
+                id: createUserId
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.result.userRemoved).to.be.true();
+        });
+    });
+
+    test('Get users, exactly 2 of them', () => {
+        return server.inject({
+            method: 'GET',
+            url: '/api/users',
+            headers: {
+                'Authorization': createdUserJwt
+            }
+        }).then((response) => {
+            expect(response.statusCode).to.equal(200);
+            expect(response.result.users).to.be.an.array();
+            expect(response.result.users.length).to.equal(2);
+        });
+    });
+
+    /*
+    test('', () => {
 
     });
 
     test('', () => {
 
-    });*/
+    });
+    */
 });
